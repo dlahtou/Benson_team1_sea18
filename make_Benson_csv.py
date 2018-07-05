be594be3734dacf8fee697060c55418727223312
Daniel Ah Tou <dlahtou@gmail.com>
2018-07-05 13:30:10 -0700

    added BensonData files to new folder, updated make_Benson

--- make_Benson_csv.py
+++ make_Benson_csv.py
@@ -1,42 +1,45 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta, time, date
+from os.path import join
 
-df1 = pd.read_csv('~/ds/metis/Benson_team1_sea18/data/turnstile_180623.txt')
+def make_Benson_csv(inputfilepath, outputfilepath):
 
-## strip whitespace in column headers
-newcolumns = df1.columns.str.strip()
-df1.columns = newcolumns
+    df1 = pd.read_csv(inputfilepath)
+    print(df1.head(5))
 
-df1['DATETIME'] = df1['DATE'] + ' ' + df1['TIME']
-df1['DATETIME'] = pd.to_datetime(df1['DATETIME'])
+    ## strip whitespace in column headers
+    newcolumns = df1.columns.str.strip()
+    df1.columns = newcolumns
 
-df1['WEEKDAY'] = df1['DATETIME'].dt.dayofweek
+    df1['DATETIME'] = df1['DATE'] + ' ' + df1['TIME']
+    df1['DATETIME'] = pd.to_datetime(df1['DATETIME'])
 
-df1['ENTRY_DIFFS'] = (df1
-                      .sort_values(by=['STATION','UNIT', 'C/A', 'SCP'])[['STATION','UNIT', 'C/A', 'SCP', 'ENTRIES', 'EXITS', 'DATETIME']]
-                      .groupby(['STATION', 'UNIT', 'C/A', 'SCP'])['ENTRIES']
-                      .diff())
-df1['EXIT_DIFFS'] = (df1
-                     .sort_values(by=['STATION','UNIT', 'C/A', 'SCP'])[['STATION','UNIT', 'C/A', 'SCP', 'ENTRIES', 'EXITS', 'DATETIME']]
-                     .groupby(['STATION', 'UNIT', 'C/A', 'SCP'])['EXITS']
-                     .diff())
+    df1['WEEKDAY'] = df1['DATETIME'].dt.dayofweek
 
-## purge values that don't make any sense in ENTRY_DIFFS and EXIT_DIFFS
-df1.loc[df1['ENTRY_DIFFS'] < 0, 'ENTRY_DIFFS'] = 0
-df1.loc[df1['ENTRY_DIFFS'] > 100000, 'ENTRY_DIFFS'] = 0
-df1.loc[df1['EXIT_DIFFS'] < 0, 'EXIT_DIFFS'] = 0
-df1.loc[df1['EXIT_DIFFS'] > 100000, 'EXIT_DIFFS'] = 0
+    df1['ENTRY_DIFFS'] = (df1
+                        .sort_values(by=['STATION','UNIT', 'C/A', 'SCP'])[['STATION','UNIT', 'C/A', 'SCP', 'ENTRIES', 'EXITS', 'DATETIME']]
+                        .groupby(['STATION', 'UNIT', 'C/A', 'SCP'])['ENTRIES']
+                        .diff())
+    df1['EXIT_DIFFS'] = (df1
+                        .sort_values(by=['STATION','UNIT', 'C/A', 'SCP'])[['STATION','UNIT', 'C/A', 'SCP', 'ENTRIES', 'EXITS', 'DATETIME']]
+                        .groupby(['STATION', 'UNIT', 'C/A', 'SCP'])['EXITS']
+                        .diff())
 
-df1['TOTAL_TRAFFIC'] = df1['EXIT_DIFFS'] + df1['ENTRY_DIFFS']
+    ## purge values that don't make any sense in ENTRY_DIFFS and EXIT_DIFFS
+    df1.loc[df1['ENTRY_DIFFS'] < 0, 'ENTRY_DIFFS'] = float('nan')
+    df1.loc[df1['ENTRY_DIFFS'] > 100000, 'ENTRY_DIFFS'] = float('nan')
+    df1.loc[df1['EXIT_DIFFS'] < 0, 'EXIT_DIFFS'] = float('nan')
+    df1.loc[df1['EXIT_DIFFS'] > 100000, 'EXIT_DIFFS'] = float('nan')
 
-def get_ddate(row):
-    if row['DATETIME'].time() == time(0,0):
-        return row['DATETIME'].date() - timedelta(days=1)
-    else:
-        return row['DATETIME'].date()
-df1['DDATE'] = df1.apply(get_ddate, axis=1)
-df1['DWEEKDAY'] = pd.to_datetime(df1['DDATE']).dt.dayofweek
+    df1['TOTAL_TRAFFIC'] = df1['EXIT_DIFFS'] + df1['ENTRY_DIFFS']
 
-df1[:1000].to_csv('~/ds/metis/Benson_team1_sea18/data/BensonData_mini.csv')
-print(df1.head(5))
+    def get_ddate(row):
+        if row['DATETIME'].time() == time(0,0):
+            return row['DATETIME'].date() - timedelta(days=1)
+        else:
+            return row['DATETIME'].date()
+    df1['DDATE'] = df1.apply(get_ddate, axis=1)
+    df1['DWEEKDAY'] = pd.to_datetime(df1['DDATE']).dt.dayofweek
+
+    df1.to_csv(outputfilepath)
---
+++ make_all_csvs.py
@@ -0,0 +1,13 @@
+from os import listdir
+from os.path import join, isfile
+from make_Benson_csv import make_Benson_csv
+
+datafolder = 'data'
+for filename in [f for f in listdir(datafolder) if (isfile(join(datafolder, f)) and f[-4:] == ".txt")]:
+    input_filepath = join(datafolder, filename)
+    output_filepath = join('BensonData', filename[-10:-4] + ".csv")
+
+    print(input_filepath)
+    print(output_filepath)
+
+    #make_Benson_csv(input_filepath, output_filepath)
